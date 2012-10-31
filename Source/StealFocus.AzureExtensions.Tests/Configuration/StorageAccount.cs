970a5199a0863a16e7beaf9ac6e901e39f615909
Callum Hibbert <callum.hibbert@gmail.com>
2012-10-31 00:14:58 +0000

    Setting dev storage.

--- Source/StealFocus.AzureExtensions.Tests/Configuration/StorageAccount.cs
+++ Source/StealFocus.AzureExtensions.Tests/Configuration/StorageAccount.cs
@@ -7,9 +7,5 @@
         public const string Name = DevelopmentStorage.AccountName;
 
         public const string Key = DevelopmentStorage.AccountKey;
-
-        // public const string Name = "bzymrktwebweurosys";
-
-        // public const string Key = "o9MPAm8nVs0wNia9QYrwY+op798kOo5nXHG8URkH0aUQy+Ro61p1mAN70egjFzPMol1/2oPHtTNbebylixdhAg==";
     }
 }
